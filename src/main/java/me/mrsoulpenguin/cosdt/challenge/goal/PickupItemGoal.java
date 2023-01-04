71aebebf1654c3e0ae7ade5600739cb23cba7b25
MrSoulPenguin <carltonj898@gmail.com>
2023-01-04 03:44:16 -0500

    Add: Pickup Item Goal

---
+++ src/main/java/me/mrsoulpenguin/cosdt/challenge/goal/PickupItemGoal.java
@@ -0,0 +1,21 @@
+package me.mrsoulpenguin.cosdt.challenge.goal;
+
+import net.minecraft.item.Item;
+
+public class PickupItemGoal extends AbstractGoal {
+
+    private final Item item;
+
+    public PickupItemGoal(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public boolean test(Object object) {
+        if (object instanceof Item newItem) {
+            return newItem.equals(this.item);
+        }
+
+        return false;
+    }
+}
