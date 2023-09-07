7acff7299bb78a4d63bac3dec64de5885461007b
MrSoulPenguin <carltonj898@gmail.com>
2023-09-07 13:42:46 -0400

    Add: ButterFingersEvent

---
+++ src/main/java/me/mrsoulpenguin/cosdt/event/punishment/ButterFingersEvent.java
@@ -0,0 +1,28 @@
+package me.mrsoulpenguin.cosdt.event.punishment;
+
+import me.mrsoulpenguin.cosdt.event.Event;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+
+public class ButterFingersEvent implements Event {
+
+    @Override
+    public ActionResult execute(PlayerEntity recipient) {
+        PlayerInventory inventory = recipient.getInventory();
+        ItemStack mainStack = inventory.getMainHandStack();
+
+        ItemEntity itemEntity = recipient.dropItem(mainStack.copy(), false, false);
+        if (itemEntity != null) {
+            mainStack.setCount(0);
+            itemEntity.setInvulnerable(true);
+            itemEntity.setNeverDespawn();
+            itemEntity.setPickupDelay(6000);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+}
