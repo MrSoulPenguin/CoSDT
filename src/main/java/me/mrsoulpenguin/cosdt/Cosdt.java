0cacfc8176af1c6575388488d9c1a44848521e57
MrSoulPenguin <carltonj898@gmail.com>
2023-09-07 22:31:07 -0400

    Update: Register commands

--- src/main/java/me/mrsoulpenguin/cosdt/Cosdt.java
+++ src/main/java/me/mrsoulpenguin/cosdt/Cosdt.java
@@ -1,10 +1,14 @@
 package me.mrsoulpenguin.cosdt;
 
+import me.mrsoulpenguin.cosdt.command.ChallengeCommand;
 import me.mrsoulpenguin.cosdt.listener.MessageSendListener;
 import me.mrsoulpenguin.cosdt.listener.PickupItemListener;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class Cosdt implements ModInitializer {
 
@@ -14,11 +18,17 @@ public class Cosdt implements ModInitializer {
 
         logger.info("Registering Listeners...");
         this.registerListeners();
+        logger.info("Registering Commands...");
+        this.registerCommands();
     }
 
     private void registerListeners() {
         new PickupItemListener();
         new MessageSendListener();
+    }
+    
+    private void registerCommands() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> new ChallengeCommand().register(dispatcher, registryAccess));
     }
 
 }
