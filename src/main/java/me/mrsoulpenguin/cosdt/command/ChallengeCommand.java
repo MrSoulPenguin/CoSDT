5ca391a24dc5a2d8158fa2364c83336d431d16a2
MrSoulPenguin <carltonj898@gmail.com>
2023-09-09 03:01:28 -0400

    Update: Add /challenge give command

---
+++ src/main/java/me/mrsoulpenguin/cosdt/command/ChallengeCommand.java
@@ -0,0 +1,61 @@
+package me.mrsoulpenguin.cosdt.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import me.mrsoulpenguin.cosdt.challenge.AbstractChallenge;
+import me.mrsoulpenguin.cosdt.challenge.ChallengeFactory;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.NbtCompoundArgumentType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class ChallengeCommand {
+
+    public void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(
+                literal("challenge")
+                        .then(literal("give")
+                                .then(argument("participant", EntityArgumentType.player())
+                                        .then(argument("nbt", NbtCompoundArgumentType.nbtCompound())
+                                                .executes(context -> execute(context, EntityArgumentType.getPlayer(context, "participant"), NbtCompoundArgumentType.getNbtCompound(context, "nbt")))
+                                        )
+                                )
+                        )
+        );
+    }
+
+    private int execute(CommandContext<ServerCommandSource> context, PlayerEntity participant, NbtCompound nbtCompound) {
+        /*  Challenge NBT example:
+            {
+                challenge: {
+                    timed: 1b,
+                    time_limit: 60,
+                    goal: {
+                        id: "cosdt:answer_question",
+                        question: "What is 2 + 2?",
+                        answer: "4"
+                    },
+                    punishment: {
+                        id: "cosdt:no_hands"
+                    },
+                    reward: {
+                        id: "cosdt:give_item",
+                          item: {
+                            id: "minecraft:diamond",
+                            Count: 1
+                        }
+                    }
+                }
+            }
+        */
+        AbstractChallenge challenge = ChallengeFactory.fromNbt(nbtCompound.getCompound("challenge"));
+        challenge.setParticipant(participant);
+        challenge.start();
+        return 1;
+    }
+
+}
