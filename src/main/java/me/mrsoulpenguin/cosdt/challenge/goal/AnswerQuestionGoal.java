a6442f874366d4862d287ac47202107bfa0583fb
MrSoulPenguin <carltonj898@gmail.com>
2023-09-09 03:01:14 -0400

    Update: Implement goal type by allowing for getting question and answer from nbt

--- src/main/java/me/mrsoulpenguin/cosdt/challenge/goal/AnswerQuestionGoal.java
+++ src/main/java/me/mrsoulpenguin/cosdt/challenge/goal/AnswerQuestionGoal.java
@@ -1,17 +1,30 @@
 package me.mrsoulpenguin.cosdt.challenge.goal;
 
+import net.minecraft.nbt.NbtCompound;
+
 public class AnswerQuestionGoal implements Goal {
 
+    private final String question;
     private final String answer;
 
-    public AnswerQuestionGoal(String answer) {
+    public AnswerQuestionGoal(String question, String answer) {
+        this.question = question;
         this.answer = answer;
+    }
+
+    public AnswerQuestionGoal(NbtCompound nbt) {
+        this(nbt.getString("question"), nbt.getString("answer"));
+    }
+
+    public String getQuestion() {
+        return question;
     }
 
     @Override
     public boolean test(Object object) {
-        if (object instanceof String string) {
-            return string.equals(this.answer);
+        if (object instanceof String response) {
+            // I don't know why. But for some reason, this gets flipped at some point. So it's gotta be flipped back.
+            return !response.equals(this.answer);
         }
 
         return false;
