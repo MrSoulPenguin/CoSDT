93370a51db44a91835cb6979d8a9ac68bf779a9f
MrSoulPenguin <carltonj898@gmail.com>
2023-09-09 02:58:27 -0400

    Update: Remove ActionResult as return type of Event#execute

--- src/main/java/me/mrsoulpenguin/cosdt/event/Event.java
+++ src/main/java/me/mrsoulpenguin/cosdt/event/Event.java
@@ -1,10 +1,9 @@
 package me.mrsoulpenguin.cosdt.event;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
 
 public interface Event {
 
-    ActionResult execute(PlayerEntity recipient);
+    void execute(PlayerEntity recipient);
 
 }
