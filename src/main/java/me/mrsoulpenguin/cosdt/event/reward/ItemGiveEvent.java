1b5c91852b8665f50e302fffff3911f70731ce3d
MrSoulPenguin <carltonj898@gmail.com>
2023-09-09 02:59:31 -0400

    Update: Implement event type by getting item from nbt compound

--- src/main/java/me/mrsoulpenguin/cosdt/event/reward/ItemGiveEvent.java
+++ src/main/java/me/mrsoulpenguin/cosdt/event/reward/ItemGiveEvent.java
@@ -3,26 +3,25 @@ package me.mrsoulpenguin.cosdt.event.reward;
 import me.mrsoulpenguin.cosdt.event.Event;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 
 public class ItemGiveEvent implements Event {
 
     private final ItemStack itemStack;
 
-    public ItemGiveEvent(ItemStack itemStack) {
-        this.itemStack = itemStack;
+    public ItemGiveEvent(NbtCompound nbt) {
+        this.itemStack = ItemStack.fromNbt(nbt.getCompound("item"));
     }
 
     @Override
-    public ActionResult execute(PlayerEntity recipient) {
+    public void execute(PlayerEntity recipient) {
         //Have to save these as the itemstack might not exist ones given to the player due to stacking.
         Text itemName = this.itemStack.getName();
         int itemAmount = this.itemStack.getCount();
 
         recipient.getInventory().offer(this.itemStack, true);
         recipient.sendMessage(((MutableText) Text.of("Obtained ")).append(itemName).append(Text.of(" x" + itemAmount)));
-        return ActionResult.SUCCESS;
     }
 }
